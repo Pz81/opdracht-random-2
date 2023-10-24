f4fe26326210f2e00153b626dcc442e91699c9c6
Pz81 <220739@student.glu.nl>
2023-10-24 11:48:03 +0200

    Opdracht random 2

---
+++ excel.py
@@ -0,0 +1,4 @@
+import pandas as pd                                                            #import pandas
+
+excelsheet = pd.read_excel(r'C:/Users/smas0/OneDrive - glu.nl/Python Module/opdracht random 2/monkey.xlsx')#open 'py_pruducts' als variabele excelsheet
+print(excelsheet)    
